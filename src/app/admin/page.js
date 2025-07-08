6b75fd7a3a9fdf479d1b451f89873d03e9172df9
sunilravulapati <sunilravulapati028@gmail.com>
2025-07-08 23:28:15 +0530

    fixed

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,8 +1,8 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
-import { connectDB } from "../../../lib/connectDB";
-import Article from "../../../models/Article";
+import { connectDB } from "@/lib/connectDB";
+import Article from "@/models/Article";
 import Link from "next/link";
 import { revalidatePath } from "next/cache";
 
